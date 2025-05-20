1b52dc36712a59d39a2e4418ade810ada8421d90
hematilldyl <d.hematillake@gmail.com>
2025-05-20 12:34:03 -0600

    Update testbest4.py

--- defeat_learners/testbest4.py
+++ defeat_learners/testbest4.py
@@ -1,27 +1,3 @@
-""""""  		  	   		  	  			  		 			     			  	 
-"""  		  	   		  	  			  		 			     			  	 
-Test best4 data generator.  (c) 2016 Tucker Balch  		  	   		  	  			  		 			     			  	 
-Copyright 2018, Georgia Institute of Technology (Georgia Tech)  		  	   		  	  			  		 			     			  	 
-Atlanta, Georgia 30332  		  	   		  	  			  		 			     			  	 
-All Rights Reserved  		  	   		  	  			  		 			     			  	 
-  		  	   		  	  			  		 			     			  	 
-Template code for CS 4646/7646  		  	   		  	  			  		 			     			  	 
-  		  	   		  	  			  		 			     			  	 
-Georgia Tech asserts copyright ownership of this template and all derivative  		  	   		  	  			  		 			     			  	 
-works, including solutions to the projects assigned in this course. Students  		  	   		  	  			  		 			     			  	 
-and other users of this template code are advised not to share it with others  		  	   		  	  			  		 			     			  	 
-or to make it available on publicly viewable websites including repositories  		  	   		  	  			  		 			     			  	 
-such as github and gitlab.  This copyright statement should not be removed  		  	   		  	  			  		 			     			  	 
-or edited.  		  	   		  	  			  		 			     			  	 
-  		  	   		  	  			  		 			     			  	 
-We do grant permission to share solutions privately with non-students such  		  	   		  	  			  		 			     			  	 
-as potential employers. However, sharing with other current or future  		  	   		  	  			  		 			     			  	 
-students of CS 7646 is prohibited and subject to being investigated as a  		  	   		  	  			  		 			     			  	 
-GT honor code violation.  		  	   		  	  			  		 			     			  	 
-  		  	   		  	  			  		 			     			  	 
------do not edit anything above this line---  		  	   		  	  			  		 			     			  	 
-"""  		  	   		  	  			  		 			     			  	 
-  		  	   		  	  			  		 			     			  	 
 import math  		  	   		  	  			  		 			     			  	 
   		  	   		  	  			  		 			     			  	 
 import numpy as np  		  	   		  	  			  		 			     			  	 
