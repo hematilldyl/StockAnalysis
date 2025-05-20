2894b3cc0f3d5d6dc6c13be5904b841b2afc1253
hematilldyl <d.hematillake@gmail.com>
2025-05-20 12:32:12 -0600

    Update marketsim.py

--- marketsim/marketsim.py
+++ marketsim/marketsim.py
@@ -1,31 +1,3 @@
-""""""  		  	   		  	  			  		 			     			  	 
-"""MC2-P1: Market simulator.  		  	   		  	  			  		 			     			  	 
-  		  	   		  	  			  		 			     			  	 
-Copyright 2018, Georgia Institute of Technology (Georgia Tech)  		  	   		  	  			  		 			     			  	 
-Atlanta, Georgia 30332  		  	   		  	  			  		 			     			  	 
-All Rights Reserved  		  	   		  	  			  		 			     			  	 
-  		  	   		  	  			  		 			     			  	 
-Template code for CS 4646/7646  		  	   		  	  			  		 			     			  	 
-  		  	   		  	  			  		 			     			  	 
-Georgia Tech asserts copyright ownership of this template and all derivative  		  	   		  	  			  		 			     			  	 
-works, including solutions to the projects assigned in this course. Students  		  	   		  	  			  		 			     			  	 
-and other users of this template code are advised not to share it with others  		  	   		  	  			  		 			     			  	 
-or to make it available on publicly viewable websites including repositories  		  	   		  	  			  		 			     			  	 
-such as github and gitlab.  This copyright statement should not be removed  		  	   		  	  			  		 			     			  	 
-or edited.  		  	   		  	  			  		 			     			  	 
-  		  	   		  	  			  		 			     			  	 
-We do grant permission to share solutions privately with non-students such  		  	   		  	  			  		 			     			  	 
-as potential employers. However, sharing with other current or future  		  	   		  	  			  		 			     			  	 
-students of CS 7646 is prohibited and subject to being investigated as a  		  	   		  	  			  		 			     			  	 
-GT honor code violation.  		  	   		  	  			  		 			     			  	 
-  		  	   		  	  			  		 			     			  	 
------do not edit anything above this line---  		  	   		  	  			  		 			     			  	 
-  		  	   		  	  			  		 			     			  	 
-Student Name: Dylan Hematillake (replace with your name)  		  	   		  	  			  		 			     			  	 
-GT User ID: dhematillake3 (replace with your User ID)  		  	   		  	  			  		 			     			  	 
-GT ID: 900897987 (replace with your GT ID)  		  	   		  	  			  		 			     			  	 
-"""  		  	   		  	  			  		 			     			  	 
-  		  	   		  	  			  		 			     			  	 
 import datetime as dt
   		  	   		  	  			  		 			     			  	 
 import numpy as np  		  	   		  	  			  		 			     			  	 
